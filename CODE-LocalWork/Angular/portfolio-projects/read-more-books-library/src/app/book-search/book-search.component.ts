1339df3fc02400557d4b5895fbe92b1f46c558b3
Aren Bruce <devtodaydevtomorrow@gmail.com>
2025-02-13 07:10:03 -0500

    Update to General Style and API Response Display

--- CODE-LocalWork/Angular/portfolio-projects/read-more-books-library/src/app/book-search/book-search.component.ts
+++ CODE-LocalWork/Angular/portfolio-projects/read-more-books-library/src/app/book-search/book-search.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -25,29 +27,35 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class BookSearchComponent {
   searchQuery: string = '';
   noResultsFound: boolean = false;
-  books: { title: string; author: string; coverUrl: string }[] = [];
+  books: { title: string; author: string; coverUrl: string, yearPublished: string }[] = [];
 
   constructor(private http: HttpClient) {}
 
   searchBooks(): void {
     const query = this.searchQuery;
-    if (!query.trim()) return;
+    if (!query.trim() || query === '') {
+      window.alert('Please enter a search query!');
+      return;
+    }
 
     const url = `https://openlibrary.org/search.json?q=${query}`;
     this.http.get<any>(url).subscribe((data) => {
+      console.log(data.docs);
       this.books = data.docs.map((doc: any) => ({
         title: doc.title,
         author: doc.author_name ? doc.author_name.join(', ') : 'unknown',
         coverUrl: doc.cover_i
           ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`
           : '',
+        yearPublished: doc.first_publish_year,
       }));
+      if (this.books.length === 0) {
+        this.noResultsFound = true;
+      }
     });
 
-    if (this.books.length === 0) {
-      this.noResultsFound = true;
-    }
+   
 
-    console.log('Search executed!', query);
+    console.log('Search executed!', query, );
   }
 }
--- CODE-LocalWork/Angular/portfolio-projects/read-more-books-library/src/app/library-home/library-home.component.ts
+++ CODE-LocalWork/Angular/portfolio-projects/read-more-books-library/src/app/library-home/library-home.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { BookSearchComponent } from '../book-search/book-search.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDivider } from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-library-home',
   standalone: true,
   imports: [
-    BookSearchComponent
+    BookSearchComponent,
+    MatDividerModule,
+    MatDivider,
   ],
   templateUrl: './library-home.component.html',
   styleUrl: './library-home.component.scss'
