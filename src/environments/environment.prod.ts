eb2959267ac1724cc4fc1538878ba41af1e323df
Aren Bruce <devtodaydevtomorrow@gmail.com>
2025-10-08 19:45:34 -0400

    correct environment variable value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://book-search-backend.fly.dev',
-    apiUrl: 'https://https://book-search-backend.fly.dev/api'
+    apiUrl: 'https://book-search-backend.fly.dev/api'
 };
